395b97f8eb0e85d39e3012ee45b8448f9a95a86c
jhonmendez17092009 <jhoasnelmendez@colegioguanenta.edu.co>
2025-02-06 16:24:33 -0500

    hgj

--- mini_calcularoda_guanentina.py
+++ mini_calcularoda_guanentina.py
@@ -1,5 +1,34 @@
 # input
+import math
+print("selecine una operacion")
+print("1 sumar")
+print("2 restar")
+print("3 multiplicar")
+print("4 dividir")
+print("5 potencia")
+print("6 logaritmo")
+
+opc = int(input("digite el numero de la operacion"))
 x = int(input("ingrese un numero"))
 y = int(input("ingrese un numero"))
 
-# processing
+if opc == 1:
+    r = x+y
+
+if opc == 2:
+    r = x-y
+
+if opc == 3:
+    r = x*y
+
+if opc == 4:
+    r = x/y
+
+if opc == 5:
+    r = x^y
+
+if opc == 6:
+    r = math.log(x,y)
+
+#output
+print("resultado: "+str(r))
